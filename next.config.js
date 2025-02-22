2a334e4ff26dd37999ebcc783070d4d67f5510b2
Shriyansh Agarwal <shriyanshagarwal1@gmail.com>
2025-02-22 16:03:17 +0530

    update

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone',
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: '/api/:path*',
+            },
+        ];
+    },
+};
+
+module.exports = nextConfig;
