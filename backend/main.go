b0451e6e3e18b29aaee91bcf99b1895f075792e7
Shriyansh Agarwal <shriyanshagarwal1@gmail.com>
2025-02-24 00:31:36 +0530

    localhost

--- backend/main.go
+++ backend/main.go
@@ -69,7 +69,7 @@ func main() {
     app := fiber.New()
 
     app.Use(cors.New(cors.Config{
-        AllowOrigins:     "localhost:3000",
+        AllowOrigins:     "http://localhost:3000",
         AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
         AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
         AllowCredentials: true,
